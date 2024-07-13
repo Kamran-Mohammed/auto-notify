9a2b8b27440d049995b3deec21dcd978267cc830
Kamran Mohammed <kamranabdulbasith@gmail.com>
2024-07-13 12:19:51 +0530

    changed folders

--- flutter/lib/home_page.dart
+++ flutter/lib/home_page.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool validateNumberPlate(String plate) {
+    // Define the regex pattern
+    final pattern = RegExp(r'^[A-Z]{2}\d{2}[A-Z]{1,3}\d{4}$');
+
+    // Test the plate against the regex pattern
+    return pattern.hasMatch(plate);
+  }
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Auto Notify Home"),
+        title: const Text("Auto Notify Home"),
       ),
       body: Form(
-        // key: _formKey,
+        key: _formKey,
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               TextFormField(
                 decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                ),
+                    hintText: 'AP09BH0417', border: OutlineInputBorder()),
                 validator: (String? value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter some text';
+                  } else if (!validateNumberPlate(value)) {
+                    return 'Wrong format entered';
                   }
                   return null;
                 },
@@ -35,9 +48,9 @@ class _HomePageState extends State<HomePage> {
                   onPressed: () {
                     // Validate will return true if the form is valid, or false if
                     // the form is invalid.
-                    // if () {
-                    // Process data.
-                    // }
+                    if (_formKey.currentState!.validate()) {
+                      // Process data.
+                    }
                   },
                   child: const Text('Submit'),
                 ),
