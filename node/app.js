e86493203c8e967f724f6463526335478b56fcc2
Zaid Mohammed <zaid.md221@gmail.com>
2024-07-15 20:21:25 +0530

    commit

--- node/app.js
+++ node/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 // const tourRouter = require("./routes/tourRoutes");
 // const userRouter = require("./routes/userRoutes");
 // const AppError = require("./utils/appError");
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
